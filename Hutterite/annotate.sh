ea7a1621e667196c4b9120c8e19951f7f179b006
smozaffari <saharvm@gmail.com>
2015-07-22 17:04:00 -0500

    run annotation script

--- Hutterite/annotate.sh
+++ Hutterite/annotate.sh
@@ -2,7 +2,7 @@
 #PBS -N annotate_bim
 #PBS -l walltime=2:00:00:00
 #PBS -l nodes=1:ppn=1
-#PBS -l mem=25gb
+#PBS -l mem=15gb
 #PBS -e annotate_bim.err
 #PBS -o annotate_bim.log
 #PBS -M smozaffari@uchicago.edu
@@ -11,7 +11,7 @@ export TMPDIR=$WORKDIR
 cd $PBS_O_WORKDIR
 export TEMP=$WORKDIR
 
-perl /group/ober-resources/users/smozaffari/Prediction/data/test/scripts/annotate.sh qc.bim
+perl /group/ober-resources/users/smozaffari/Prediction/data/test/PrediXcan/Hutterite/annotate.pl qcfiles_rsids_hapmapSNPs.map
 
 
 
