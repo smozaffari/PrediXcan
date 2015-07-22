069b0cde1e4976d2d03ef15485040f6ff8a21022
smozaffari <saharvm@gmail.com>
2015-07-22 16:59:02 -0500

    running HT_genotypescript_2.R to make dosage files

--- Hutterite/HT_gtype.sh
+++ Hutterite/HT_gtype.sh
@@ -1,6 +1,6 @@
  #!/bin/bash
 #PBS -N HT_gtype
-#PBS -l walltime=2:00:00:00
+#PBS -l walltime=4:00:00:00
 #PBS -l nodes=1:ppn=1
 #PBS -l mem=15gb
 #PBS -e HT_gtype.err
@@ -13,7 +13,7 @@ export TEMP=$WORKDIR
 
 module load R
 
-Rscript /group/ober-resources/users/smozaffari/Prediction/data/test/PrediXcan/Hutterite/HT_genotypescript.R
+Rscript /group/ober-resources/users/smozaffari/Prediction/data/test/PrediXcan/Hutterite/HT_genotypescript_2.R
 
 
 
