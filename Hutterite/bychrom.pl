f3ed676898c9a0ffa26dcb7ae367733507092723
smozaffari <saharvm@gmail.com>
2015-07-31 16:33:59 -0500

    using correct updated file: NAfilledin_recode_all.ped to make chr.ped files

--- Hutterite/bychrom.pl
+++ Hutterite/bychrom.pl
@@ -29,7 +29,7 @@ close(BIM);
 foreach my $str ( sort keys %count) {
     my $a = $start{$str};
     my $b = $stop{$str};
-    my $cmd = ('cut -f'.$a.'-'.$b.' -d" " NAfilledin.ped > chr'.$str.'.ped \n');
+    my $cmd = ('cut -f'.$a.'-'.$b.' -d" " NAfilledin_recode_all.ped > chr'.$str.'.ped \n');
 #    my $cmd = ('cut -f'.$a.'-'.$b.' -d" " qcfiles_rsids_hapmapSNPs.ped > chr'.$str.'.ped \n');
 #    print ($cmd);
 #    print ("\n");
