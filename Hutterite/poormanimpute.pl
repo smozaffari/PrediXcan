e08a0226d2cb64ed12ae448bd65c4e04246381ef
smozaffari <saharvm@gmail.com>
2015-07-31 16:36:19 -0500

    imputes most common genotype using file commongenotype

--- Hutterite/poormanimpute.pl
+++ Hutterite/poormanimpute.pl
@@ -5,18 +5,12 @@ use warnings;
 
 my %af;
 
-open (BIM, "/group/ober-resources/users/smozaffari/Prediction/data/test/correctgeno") || die "nope: $!";
-my $counter =7;
+open (BIM, "/group/ober-resources/users/smozaffari/Prediction/data/test/commongenotype") || die "nope: $!";
 while (my $line = <BIM>) {
     chomp $line;
     my @line = split "\t", $line;
-    my $snp = $line[1];
-    if ($af{$snp}) {
-	print "duplicated: $snp";
-    }
-    $af{$snp} = $line[6];   
-    $af{$counter} = 2*$line[6];
-    $counter++;
+    my $snp = $line[0];
+    $af{$snp} = $line[1];   
 }
 
 #open (PED, "/group/ober-resources/users/smozaffari/Prediction/data/test/qcfiles_rsids_hapmapSNPs_geno0.15.ped") || die "nope: $!";
@@ -25,9 +19,9 @@ open (PED, "/group/ober-resources/users/smozaffari/Prediction/data/test/recode_a
 open (NEW, ">NAfilledin_recode_all.ped") || die "nope: $!";
 while (my $line = <PED>) {
     my @line = split " ", $line;
-    my $count2 = 1;
+    my $count2 = 0;
     foreach my $geno (@line) {
-	if ($count2 >=7 ){
+	if ($count2 >=6 ){
 	    if ($geno =~ /NA/) {
 		print NEW ("$af{$count2} ");
 	    } else {
